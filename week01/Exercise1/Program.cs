70179380413bb6b3315fd2bc92c67bc15b68b590
Marco Parra <marco.a.parra11@gmail.com>
2025-05-10 05:48:38 -0600

    james bond completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your firstname? "); string firstName = Console.ReadLine();
+        Console.Write("What is your lastname? "); string lastName = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {firstName}, {firstName} {lastName}.");
     }
 }
+
