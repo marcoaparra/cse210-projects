38741af088dba4250a8f66c394cf63c38bde047e
Marco Parra <marco.a.parra11@gmail.com>
2025-05-15 16:09:30 -0600

    completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,28 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "General helper";
+        job1._company = "Air Temp";
+        job1._startYear = 2021;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Technical support specialist";
+        job2._company = "Libera";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Marco Parra";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
