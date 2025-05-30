8f0a9fc10a46fd8d4ee2db176c72f9632bc59bcd
Marco Parra <marco.a.parra11@gmail.com>
2025-05-29 19:01:30 -0600

    classes

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,21 @@
+using System.Security.Cryptography;
+
+public class Journal
+{
+    public List<Entry> _entries;
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+    public void LoadFromFile(string file)
+    {}
+}
