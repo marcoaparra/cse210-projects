5c72448631e87b0e0bf1f4dc23581f06b41779e3
Marco Parra <marco.a.parra11@gmail.com>
2026-01-17 01:37:59 -0600

    week 2 second step

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,54 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage? ");
+        string number = Console.ReadLine();
+        double percentage = double.Parse(number);
+
+            if (percentage >= 90)
+            {
+                Console.WriteLine("Your grade is A.");
+                if (percentage >= 90)
+                {
+                    Console.WriteLine("Congratulations! You passed the course!");
+                }
+            }
+            else if (percentage >= 80)
+            {
+                Console.WriteLine("Your grade is B.");
+                if (percentage >= 80)
+                {
+                    Console.WriteLine("Congratulations! You passed the course!");
+                }
+            }
+            else if (percentage >= 70)
+            {
+                Console.WriteLine("Your grade is C.");
+                if (percentage >= 70)
+                {
+                    Console.WriteLine("Congratulations! You passed the course!");
+                }
+            }
+            else if (percentage >= 60)
+            {
+                Console.WriteLine("Your grade is D.");
+                if (percentage <= 69)
+                {
+                    Console.WriteLine("I'm sorry, you did not pass the course.");
+                }
+            }
+            else if (percentage < 60)
+            {
+                Console.WriteLine("Your grade is F.");
+                 if (percentage <= 69)
+                {
+                    Console.WriteLine("I'm sorry, you did not pass the course. Keep trying! You can do it!");
+                }
+            }       
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a numeric grade percentage.");
+            }
     }
 }
