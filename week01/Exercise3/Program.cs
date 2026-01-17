3d1c559c72e9aa4c9b7bc7501172ddc8c25a1d1f
Marco Parra <marco.a.parra11@gmail.com>
2026-01-17 13:52:14 -0600

    week one csharp 3 part 1 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        int magicNumber = 6;
+        Console.WriteLine($"What is the magic number? {magicNumber}");
+        Console.Write($"What is your guess? ");
+        string yourGuess = Console.ReadLine();
+        int yourGuessNumber = int.Parse(yourGuess);
+
+        if (yourGuessNumber < magicNumber)
+        {
+            Console.WriteLine("Lower. ");
+        }
+        else if (yourGuessNumber == magicNumber)
+        {
+            Console.WriteLine("You guessed it! ");
+        } 
+        else
+        {
+            Console.WriteLine("Higher. ");
+        }
+
+        
     }
 }
