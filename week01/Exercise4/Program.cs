d3c3ba3f7b30a10fed82809cd254e1d0d3b41fb5
Marco Parra <marco.a.parra11@gmail.com>
2026-01-17 17:06:23 -0600

    week 1 exercise four completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers = new List<int>();
+
+        int addedNumbers = -1;
+
+        Console.WriteLine("Please write some integer numbers. When you type zero, the program will stop. ");
+        
+        //Loop from here.
+
+        while (addedNumbers != 0)
+        {
+        Console.Write("Enter number: ");
+        string userNumber = Console.ReadLine();
+        addedNumbers = int.Parse(userNumber);
+
+        if (addedNumbers != 0)
+            {
+                numbers.Add(addedNumbers);
+            }
+        }
+        //numbers.Add(addedNumbers); "Close, but not yet." 
+
+        // Console.WriteLine(numbers); Shows: System.Collections.Generic.List`1[System.Int32]
+
+        //Console.WriteLine(numbers.Count);
+        // Shows: The number of numbers that I add.
+
+        int sum = 0;
+        foreach (int number in numbers)
+            {
+                sum += number;
+            }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int maxNumber = numbers [0];
+
+        foreach (int number in numbers)
+            {
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+            }
+        
+        Console.WriteLine($"The max is: {maxNumber} ");
     }
 }
