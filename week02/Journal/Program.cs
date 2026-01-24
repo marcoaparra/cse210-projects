fe863e9ce2fdd754403a187d66093cc0daacae8b
Marco Parra <marco.a.parra11@gmail.com>
2026-01-24 14:10:57 -0600

    creating journal programm

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
-    }
+        Console.WriteLine("");
+        Console.WriteLine("Please select one of the following choices: ");
+        Console.WriteLine("1. Write. ");
+        Console.WriteLine("2. Display. ");
+        Console.WriteLine("3. Load. ");
+        Console.WriteLine("4. Save. ");
+        Console.WriteLine("5. Quit. ");
+        Console.WriteLine("");
+        Console.Write("What would you like to do? ");
+        string answer = Console.ReadLine();
+        int answerNumber = int.Parse(answer);
+
+        if (answerNumber == 1 )
+        {
+            Console.WriteLine("Good.");
+        }
+    }   
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Scripture
+{
+private string scriptureStorage;
+
+public void Display()
+    {
+        Console.WriteLine();
+    }
+
+} 
